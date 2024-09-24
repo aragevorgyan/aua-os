e6f5406dca06392fdbec6c8a6d0da955d64a4944
aragevorgyan <aragev@google.com>
2024-09-24 17:00:14 +0400

    Linking sources, intermediate files, and executables

---
+++ src/linking/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define GREETING "Hello, World!"
+
+
+int main() {
+    printf("%s\n", GREETING);
+    return 0;
+}
---
+++ src/linking/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "math_operations.h"
+
+int main() {
+    int result = add(3, 4);
+    printf("Result of addition: %d\n", result);
+
+    while(1);
+    return 0;
+}
